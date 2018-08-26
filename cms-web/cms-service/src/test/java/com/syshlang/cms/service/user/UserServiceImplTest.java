0508885cec5616b2f4be909d5e11dd2f91ed881a
syshlang <158990605@qq.com>
2018-08-26 20:17:23 +0800

    cms-service:JUnit Test

--- cms-web/cms-service/src/test/java/com/syshlang/cms/service/user/UserServiceImplTest.java
+++ cms-web/cms-service/src/test/java/com/syshlang/cms/service/user/UserServiceImplTest.java
@@ -18,8 +18,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.*;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({
         "classpath*:META-INF/spring/*.xml"
