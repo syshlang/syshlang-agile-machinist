74bda23672ec09c24bfb36b1fa30f61111875bc8
syshlang <158990605@qq.com>
2018-08-28 00:35:25 +0800

    shiro 实例工厂方法的方式配置权限过滤抽象类ShiroFilterChainDefinitionMapBuilder

--- machinist-authority/machinist-authority-shiro/src/main/java/com/syshlang/authority/shiro/factory/ShiroFilterChainDefinitionMapBuilder.java
+++ machinist-authority/machinist-authority-shiro/src/main/java/com/syshlang/authority/shiro/factory/ShiroFilterChainDefinitionMapBuilder.java
@@ -15,7 +15,13 @@ import java.util.LinkedHashMap;
  * 实例工厂方法的方式配置权限过滤抽象类,由需要调用的系统实现方法
  * @author sunys
  */
-public abstract  class ShiroFilterChainDefinitionMapBuilder{
+public class ShiroFilterChainDefinitionMapBuilder{
 
-    public abstract LinkedHashMap<String, String> buildFilterChainDefinitionMap();
+    public LinkedHashMap<String, String> buildFilterChainDefinitionMap(){
+        LinkedHashMap<String, String> map = new LinkedHashMap<String, String>();
+        map.put("/login", "anon");
+        map.put("/logout", "logout");
+        map.put("/**", "sessionForceLogout,authc");
+        return map;
+    }
 }
