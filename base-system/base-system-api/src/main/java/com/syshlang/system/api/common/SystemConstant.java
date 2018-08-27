d5d5852771ffede490dba3d40c14a4e82d2be4bd
syshlang <158990605@qq.com>
2018-08-27 21:17:28 +0800

    datebase model

--- base-system/base-system-api/src/main/java/com/syshlang/system/api/common/SystemConstant.java
+++ base-system/base-system-api/src/main/java/com/syshlang/system/api/common/SystemConstant.java
@@ -17,9 +17,9 @@ import com.syshlang.api.common.ApiConstant;
 public class SystemConstant extends ApiConstant {
     public static final  Class CLASS = SystemConstant.class;
 
-    public static final String RESOURCES_EMPTY_USERNAME = "cms.user.login.emptyUsername";
+    public static final String RESOURCES_EMPTY_USERNAME = "system.user.login.emptyUsername";
     public static final String EMPTY_USERNAME = "用户名不能为空!";
 
-    public static final String RESOURCES_EMPTY_PASSWORD = "cms.user.login.emptyPassword";
+    public static final String RESOURCES_EMPTY_PASSWORD = "system.user.login.emptyPassword";
     public static final String EMPTY_PASSWORD = "密码不能为空!";
 }
