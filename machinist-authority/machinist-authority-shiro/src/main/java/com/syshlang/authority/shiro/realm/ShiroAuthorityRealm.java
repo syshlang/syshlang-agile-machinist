0ed1c50159e06201ca76d9663196df0080ed989e
syshlang <sunyslang@gmail.com>
2018-08-30 18:47:05 +0800

    shiro  登录验证加密方式

--- machinist-authority/machinist-authority-shiro/src/main/java/com/syshlang/authority/shiro/realm/ShiroAuthorityRealm.java
+++ machinist-authority/machinist-authority-shiro/src/main/java/com/syshlang/authority/shiro/realm/ShiroAuthorityRealm.java
@@ -13,10 +13,14 @@ import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.credential.CredentialsMatcher;
+import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.crypto.hash.SimpleHash;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.ByteSource;
 
 /**
  * @author sunys
@@ -29,10 +33,38 @@ public class ShiroAuthorityRealm extends AuthorizingRealm {
         return new SimpleAuthorizationInfo();
     }
 
+    /**
+     * 登录时认证
+     * @param authenticationToken
+     * @return
+     * @throws AuthenticationException
+     */
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
         String username = (String) authenticationToken.getPrincipal();
         String password = new String((char[]) authenticationToken.getCredentials());
+        CredentialsMatcher credentialsMatcher = getCredentialsMatcher();
+        if (credentialsMatcher != null){
+            HashedCredentialsMatcher hashedCredentialsMatcher = (HashedCredentialsMatcher) credentialsMatcher;
+            String hashAlgorithmName = hashedCredentialsMatcher.getHashAlgorithmName();
+            int hashIterations = hashedCredentialsMatcher.getHashIterations();
+        }
+
+        AuthenticationInfo authenticationInfo  = new SimpleAuthenticationInfo(username, password, getName());
+        if (credentialsMatcher != null){
+            boolean doCredentialsMatch = credentialsMatcher.doCredentialsMatch(authenticationToken, authenticationInfo);
+
+        }
         return new SimpleAuthenticationInfo(username, password, getName());
+    }
+
+    public static void main(String[] args) {
+        String hashAlgorithmName = "MD5";
+        Object credentials = "123456";
+        Object salt = ByteSource.Util.bytes("user");;
+        int hashIterations = 1024;
+
+        Object result = new SimpleHash(hashAlgorithmName, credentials, salt, hashIterations);
+        System.out.println(result);
     }
 }
