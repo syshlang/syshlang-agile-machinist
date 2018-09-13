f6014e46a21e219635a2c3661d70becbab80555b
syshlang <sunyslang@gmail.com>
2018-09-13 16:38:54 +0800

    sql

--- base-system/system-document/database.sql
+++ base-system/system-document/database.sql
@@ -16,6 +16,39 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `sys_user`
+--
+
+DROP TABLE IF EXISTS `sys_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sys_user` (
+  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `username` varchar(20) NOT NULL COMMENT '帐号',
+  `password` varchar(32) NOT NULL COMMENT '密码MD5(密码+盐)',
+  `salt` varchar(32) DEFAULT NULL COMMENT '盐',
+  `real_name` varchar(20) DEFAULT NULL COMMENT '姓名',
+  `nick_name` varchar(20) DEFAULT NULL COMMENT '昵称',
+  `avatar` varchar(150) DEFAULT NULL COMMENT '头像',
+  `phone` varchar(20) DEFAULT NULL COMMENT '电话',
+  `email` varchar(50) DEFAULT NULL COMMENT '邮箱',
+  `sex` varchar(4) DEFAULT '0' COMMENT '性别(0:未知,1:男,2:女)',
+  `locked` varchar(4) DEFAULT '0' COMMENT '状态(0:正常,1:锁定)',
+  `create_time` bigint(20) DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sys_user`
+--
+
+LOCK TABLES `sys_user` WRITE;
+/*!40000 ALTER TABLE `sys_user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sys_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `sys_user_online`
 --
 
@@ -24,7 +57,7 @@ DROP TABLE IF EXISTS `sys_user_online`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `sys_user_online` (
   `session_id` varchar(100) NOT NULL DEFAULT '' COMMENT '用户会话id',
-  `user_id` int(10) unsigned NOT NULL COMMENT '编号',
+  `user_id` int(10) unsigned DEFAULT NULL COMMENT '编号',
   `code` varchar(100) NOT NULL COMMENT '加密code',
   `ipaddr` varchar(50) DEFAULT '' COMMENT '登录IP地址',
   `login_location` varchar(255) DEFAULT '' COMMENT '登录地点',
@@ -56,4 +89,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-09-13 16:32:05
+-- Dump completed on 2018-09-13 16:37:47
