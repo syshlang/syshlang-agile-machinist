fc4751ccd53a67a1e6b73ff62a6967dc700541de
syshlang <sunyslang@gmail.com>
2018-09-21 13:26:24 +0800

    swagger Api

--- base-system/base-system-consumer/src/main/java/com/syshlang/system/consumer/user/UserController.java
+++ base-system/base-system-consumer/src/main/java/com/syshlang/system/consumer/user/UserController.java
@@ -21,6 +21,10 @@ import com.syshlang.system.sso.common.api.ShiroConstant;
 import com.syshlang.system.sso.common.api.ShiroController;
 import com.syshlang.system.model.online.entity.UserOnline;
 import com.syshlang.system.model.user.entity.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.shiro.SecurityUtils;
@@ -42,6 +46,7 @@ import javax.servlet.http.HttpSession;
 /**
  * @author sunys
  */
+@Api(value = "单点登录",tags = "单点登录/注销控制层")
 @Controller
 @RequestMapping("system/user")
 public class UserController extends ShiroController {
@@ -53,19 +58,24 @@ public class UserController extends ShiroController {
     @Autowired
     private UserOnlineService userOnlineService;
 
-    @RequestMapping("index.html")
+	@ApiOperation(value = "登录页",notes = "跳转到登录页面")
+    @RequestMapping(value = "index.html",method = RequestMethod.GET)
     public String index(HttpServletRequest request){
         return "login/index";
     }
 
-	
-	@RequestMapping(value="login.json",method = RequestMethod.POST)
 	@ResponseBody
+	@RequestMapping(value="login.json",method = RequestMethod.POST)
+	@ApiOperation(value = "登录提交",notes = "用户登录请求")
+	@ApiImplicitParams({
+			@ApiImplicitParam(paramType="query", name = "username", value = "用户名称", required = true, dataType = "String"),
+			@ApiImplicitParam(paramType="query", name = "password", value = "用户密码", required = true, dataType = "String"),
+			@ApiImplicitParam(paramType="query", name = "rememberMe", value = "是否记住", required = false, dataType = "String")
+	})
 	public BaseResult login(HttpServletRequest request,
 							@RequestParam(value="username",defaultValue="",required=true) String username,
 							@RequestParam(value="password",defaultValue="",required=true) String password,
                             @RequestParam(value ="rememberMe",defaultValue="",required=false) String rememberMe){
-
 		BaseResult result = new BaseResult(BaseResultCode.SUCCESS);
         if (StringUtils.isBlank(username)){
             return new SystemResult(SystemResultCode.EMPTY_USERNAME);
@@ -118,7 +128,7 @@ public class UserController extends ShiroController {
 		User user = (User) session.getAttribute("user");
 		if(null!=user){
 			LOGGER.info("退出成功");
-			session.removeAttribute("user");;
+			session.removeAttribute("user");
 			result =new BaseResult(BaseResultCode.SUCCESS);
 		}else{
 			result =new BaseResult(BaseResultCode.ERROR);
