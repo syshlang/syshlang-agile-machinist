4e8c6e8b8a39fc9b63ec30a289f8160e1ee7c72a
syshlang <sunyslang@gmail.com>
2018-10-12 14:53:53 +0800

    WebInterceptor

--- base-system/base-system-consumer/src/main/java/com/syshlang/system/web/interceptor/WebInterceptor.java
+++ base-system/base-system-consumer/src/main/java/com/syshlang/system/web/interceptor/WebInterceptor.java
@@ -12,6 +12,7 @@ package com.syshlang.system.web.interceptor;
 import com.syshlang.common.util.enumutil.EnumUtil;
 import com.syshlang.common.util.properties.PropertiesFileUtil;
 import com.syshlang.system.api.common.SystemConstant;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
@@ -34,12 +35,27 @@ public class WebInterceptor  extends HandlerInterceptorAdapter {
         if (null != request.getHeader("X-Requested-With") && "XMLHttpRequest".equalsIgnoreCase(request.getHeader("X-Requested-With"))) {
             return true;
         }
-        String appName = PropertiesFileUtil.getInstance().get("app.name");
+        String systemName = PropertiesFileUtil.getInstance().get("system.name");
+        if (StringUtils.isBlank(systemName)){
+            throw  EnumUtil.productException(SystemConstant.SYSTEM_EXCEPTION.EXCEPTION_PARAM,new String[]{"system.name"},null);
+        }
+        request.setAttribute("system_name", systemName);
+
         String profileEnv = PropertiesFileUtil.getInstance().get("profile.env");
-        String uiPath = PropertiesFileUtil.getInstance().get("ui.path");
-        request.setAttribute("appName", appName);
+        if (StringUtils.isBlank(systemName)){
+            throw  EnumUtil.productException(SystemConstant.SYSTEM_EXCEPTION.EXCEPTION_PARAM,new String[]{"profile.env"},null);
+        }
         LOGGER.info("当前系统环境："+EnumUtil.getEnumDesc(SystemConstant.SYSTEM_ENV.class,profileEnv));
         request.setAttribute("system_env", EnumUtil.getEnumDesc(SystemConstant.SYSTEM_ENV.class,profileEnv));
+
+        String uiPath = PropertiesFileUtil.getInstance().get("ui.path");
+        if (StringUtils.isBlank(systemName)){
+            throw  EnumUtil.productException(SystemConstant.SYSTEM_EXCEPTION.EXCEPTION_PARAM,new String[]{"ui.path"},null);
+        }
+
+        String contextPath = request.getContextPath();
+        request.setAttribute("basePath", contextPath);
+        uiPath = contextPath+"/"+uiPath;
         request.setAttribute("uiPath", uiPath);
         return true;
     }
